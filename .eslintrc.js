eef1c7e88f8566732a6fe3d4239caa27cc6b71b1
Haley Hamill <hamillhr@gmail.com>
2025-04-13 21:34:41 -0700

    Updates

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
